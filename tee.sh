ed267f571f2909575a2ca91acf2c591b9bbed1b3
Eddie Chen <122704850+eddiemaximchen@users.noreply.github.com>
2023-02-14 15:07:01 +0800

    Create tee.sh

---
+++ tee.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo source /home/bigred/alpine.bash | sudo tee -a /etc/profile &>/dev/null
+# print the result 'source /home/bigred/alpine.bash' append to the end o etc/profile
+#etc/profile is a file as well as a program which will be the fist one execute after id/password login 
