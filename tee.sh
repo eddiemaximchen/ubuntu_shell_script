d94ef6a7ca0b259247cebdb76ee243afb53c45a1
Eddie Chen <122704850+eddiemaximchen@users.noreply.github.com>
2023-02-14 15:08:45 +0800

    Update tee.sh

--- tee.sh
+++ tee.sh
@@ -2,3 +2,4 @@
 echo source /home/bigred/alpine.bash | sudo tee -a /etc/profile &>/dev/null
 # print the result 'source /home/bigred/alpine.bash' append to the end o etc/profile
 #etc/profile is a file as well as a program which will be the fist one execute after id/password login 
+# etc/profile is like the batch file you put in the start folder in windows
